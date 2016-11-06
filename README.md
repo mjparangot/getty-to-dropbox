b68759a467a69f4eb9a399042bbc06feea946a47
Matthew Parangot <matthew.parangot@turner.com>
2016-11-06 18:35:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# getty-to-dropbox
---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Hapi = require('hapi'),
+      getty = require('./lib/getty'),
+      dropbox = require('./lib/dropbox'),
+      server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: 5000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+        return reply('hello world');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path:'/api/getty/{phrase}',
+    handler: function (request, reply) {
+        getty.getImages(request.params.phrase).then(function (data) {
+            reply(data);
+        });
+    }
+});
+
+// Start the server
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+
+    dropbox.deleteImages('/Arsenal-Images');
+    dropbox.uploadImages('stuart macfarlane');
+});
